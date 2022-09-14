4f1608fbdcdd6f921a0642f3a215a5b53ce684d4
deralph <jraphael441@gmail.com>
2022-09-14 00:21:25 -0700

    workeed on home page

--- src/components/webauthn/Home.js
+++ src/components/webauthn/Home.js
@@ -7,12 +7,14 @@ const Home = ({ attendance, admin }) => {
   return (
     <>
       <section className={styles.section}>
+        <img className={styles.imgs} src="/student.jpg" alt="student" />
         <div className={styles.left}>
+          <img src="/student.jpg" alt="student" />
           <div>
             <p style={{ display: "flex", justifyContent: "center" }}>
               <FaBookOpen className={styles.book} />
             </p>
-            
+
             <h2>attendance management system</h2>
             <p>
               Easy and reliable system for taking attendance with the use of
